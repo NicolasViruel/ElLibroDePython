70d3001fba877f0f2f6b905cceef8e6d0d45b368
nicolasviruel <nicolasviruel@gmail.com>
2024-05-21 00:24:50 -0300

    Se sube el proyecto

---
+++ holamundo.py
@@ -0,0 +1,35 @@
+print("Hola mundo")
+x = 5
+print(x)
+
+# Declaramos las variables a, b
+# y asignamos dos valores
+
+# a = 3
+# b = 7
+# print(a+b)
+
+a= 10
+if a == 10:
+    print("Es 10")
+else:
+    print("No es 10")
+
+# Definimos una variable x con una cadena
+x = "El valor de  (a+b)*c es"
+
+# Podemos realizar multiples asignaciones
+a, b , c = 4, 3, 2
+
+# Realizamos unas operaciones con a,b,c 
+d = ( a+b )*c
+
+
+# Definimos una variable booleana
+imprimir = True
+
+# Si imprimir, print()
+if imprimir: 
+    print(x,d)
+
+# Salida: El valor de (a+b)*c es 14
